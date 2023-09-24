c306c61551b27df00fce633d8bc7c29e31e0620f
Ruben <ruben@urano.group>
2023-09-24 10:41:17 -0500

    update adapters

--- customer-infrastructure/customer-infrastructure-database/src/main/java/pe/ibk/cpe/customer/infrastructure/database/mysql/DeleteCustomerAdapter.java
+++ customer-infrastructure/customer-infrastructure-database/src/main/java/pe/ibk/cpe/customer/infrastructure/database/mysql/DeleteCustomerAdapter.java
@@ -2,10 +2,14 @@ package pe.ibk.cpe.customer.infrastructure.database.mysql;
 
 import org.springframework.stereotype.Component;
 import pe.ibk.cpe.customer.domain.core.entity.Customer;
+import pe.ibk.cpe.customer.domain.core.valueobject.CustomerId;
 import pe.ibk.cpe.customer.domain.service.ports.outbound.DeleteCustomerPort;
 import pe.ibk.cpe.customer.infrastructure.database.mysql.repository.CustomerEntity;
 import pe.ibk.cpe.customer.infrastructure.database.mysql.repository.CustomerEntityMapper;
 import pe.ibk.cpe.customer.infrastructure.database.mysql.repository.CustomerRepository;
+import pe.ibk.cpe.dependencies.exception.DomainException;
+
+import java.util.Optional;
 
 @Component
 public class DeleteCustomerAdapter implements DeleteCustomerPort {
@@ -19,10 +23,14 @@ public class DeleteCustomerAdapter implements DeleteCustomerPort {
     }
 
     @Override
-    public boolean apply(Customer customer) {
-        CustomerEntity entity = customerEntityMapper.map(customer);
+    public boolean apply(CustomerId customerId) {
+        Optional<CustomerEntity> optionalEntity = customerRepository.findById(customerId.getValue().toString());
+
+        if (!optionalEntity.isPresent())
+            throw new DomainException("No found customer, id : " + customerId.getValue());
+
         try {
-            customerRepository.delete(entity);
+            customerRepository.delete(optionalEntity.get());
             return true;
         } catch (Exception ex) {
             return false;
--- customer-infrastructure/customer-infrastructure-database/src/main/java/pe/ibk/cpe/customer/infrastructure/database/mysql/GetCustomerAdapter.java
+++ customer-infrastructure/customer-infrastructure-database/src/main/java/pe/ibk/cpe/customer/infrastructure/database/mysql/GetCustomerAdapter.java
@@ -2,10 +2,12 @@ package pe.ibk.cpe.customer.infrastructure.database.mysql;
 
 import org.springframework.stereotype.Component;
 import pe.ibk.cpe.customer.domain.core.entity.Customer;
+import pe.ibk.cpe.customer.domain.core.valueobject.CustomerId;
 import pe.ibk.cpe.customer.domain.service.ports.outbound.GetCustomerPort;
 import pe.ibk.cpe.customer.infrastructure.database.mysql.repository.CustomerEntity;
 import pe.ibk.cpe.customer.infrastructure.database.mysql.repository.CustomerEntityMapper;
 import pe.ibk.cpe.customer.infrastructure.database.mysql.repository.CustomerRepository;
+import pe.ibk.cpe.dependencies.exception.DomainException;
 
 import java.util.Optional;
 
@@ -21,10 +23,10 @@ public class GetCustomerAdapter implements GetCustomerPort {
     }
 
     @Override
-    public Customer apply(String id) {
-        Optional<CustomerEntity> optionalEntity = customerRepository.findById(id);
+    public Customer apply(CustomerId customerId) {
+        Optional<CustomerEntity> optionalEntity = customerRepository.findById(customerId.getValue().toString());
 
-        CustomerEntity entity = optionalEntity.orElseThrow(() -> new RuntimeException("Not found customer with id: " + id));
+        CustomerEntity entity = optionalEntity.orElseThrow(() -> new DomainException("Not found customer, id : " + customerId));
 
         return customerEntityMapper.map(entity);
     }
