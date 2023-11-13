1f4ddaee59646de9914662e87ef80cf8254d9f0d
rramos <rramosq@intercorp.com.pe>
2023-11-13 14:14:06 -0500

    remove bean log

--- customer-container/src/main/java/pe/ibk/cpe/customer/container/CustomerMicroApplication.java
+++ customer-container/src/main/java/pe/ibk/cpe/customer/container/CustomerMicroApplication.java
@@ -2,11 +2,9 @@ package pe.ibk.cpe.customer.container;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration(exclude = {
@@ -28,16 +26,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class CustomerMicroApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(CustomerMicroApplication.class, args);
-
-        print(applicationContext);
-    }
-
-    private static void print(ApplicationContext applicationContext) {
-        String[] beans = applicationContext.getBeanDefinitionNames();
-        for (int i = 0; i < beans.length; i++) {
-            System.out.println((1 + i) + " ::: " + beans[i]);
-        }
+        SpringApplication.run(CustomerMicroApplication.class, args);
     }
 }
 
