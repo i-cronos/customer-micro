d7462d3302b986a7cf22f5a946280898d451165f
Ruben <ruben@urano.group>
2023-11-13 12:05:48 -0500

    update customer path

--- customer-application/src/main/java/pe/ibk/cpe/customer/application/rest/controller/customer/CustomerRestController.java
+++ customer-application/src/main/java/pe/ibk/cpe/customer/application/rest/controller/customer/CustomerRestController.java
@@ -3,9 +3,6 @@ package pe.ibk.cpe.customer.application.rest.controller.customer;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,22 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 import pe.ibk.cpe.customer.domain.service.ports.inbound.customer.CreateCustomerUseCase;
 import pe.ibk.cpe.customer.domain.service.services.customer.dto.CreateCustomerRequest;
 import pe.ibk.cpe.customer.domain.service.services.customer.dto.CreateCustomerResponse;
-import pe.ibk.cpe.dependencies.common.security.SystemUser;
-
-import java.util.Objects;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/protected/customer/v1.0")
+@RequestMapping("/api/public/customer/v1.0")
 @AllArgsConstructor
 public class CustomerRestController {
     private final CreateCustomerUseCase createCustomerUseCase;
 
     @PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CreateCustomerResponse> create(@RequestBody CreateCustomerRequest createCustomerRequest) {
-        SystemUser systemUser = (SystemUser) SecurityContextHolder.getContext().getAuthentication();
-        log.info("System user id : {}", systemUser.getUserId());
         return ResponseEntity.ok(createCustomerUseCase.apply(createCustomerRequest));
     }
 }
